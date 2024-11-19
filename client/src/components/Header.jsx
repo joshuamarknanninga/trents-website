3cd48a97d641927e691122e99e79f941a26a350a
Joshua Nanninga <jmarknanninga@gmail.com>
2024-11-19 15:15:54 -0600

    revamped css, instgalled react icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Menu from './Menu';
 import './Header.scss';
 
-const Header = () => (
-  <header className="header">
-    <h1 className="header-title">Sustainable Process</h1>
-    <nav className="header-menu">
-      <ul>
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#process">Process</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <header className={`header ${scrolled ? 'scrolled' : ''}`}>
+      <h1 className="header-title">Sustainable Process</h1>
+      <Menu />
+    </header>
+  );
+};
 
 export default Header;
