6c59b5f67fc447bb9709dfeff4eb42a23f3017e8
Joshua Nanninga <jmarknanninga@gmail.com>
2024-11-23 15:23:14 -0600

    removed unneccesary files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+// client/tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in-down': 'fadeInDown 1s ease-out',
+        'fade-in-up': 'fadeInUp 1s ease-out',
+      },
+      keyframes: {
+        fadeInDown: {
+          '0%': { opacity: 0, transform: 'translateY(-20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
