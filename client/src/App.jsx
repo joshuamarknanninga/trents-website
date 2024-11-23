b7c746ba8f3b5faebfacdbcedb4ee7b67d843861
Joshua Nanninga <jmarknanninga@gmail.com>
2024-11-23 16:31:05 -0600

    corrected json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api') // Using proxy to avoid specifying full URL
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching data:', error));
+    fetch(`${process.env.REACT_APP_API_URL}/api`)
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message))
+    .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   return (
